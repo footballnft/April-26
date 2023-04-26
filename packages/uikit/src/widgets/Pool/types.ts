825c964573acb81ecd7d3435453af92707c1270f
footballnft <85647902+footballnft@users.noreply.github.com>
2023-04-26 11:43:18 +0100

    first commit

---
+++ packages/uikit/src/widgets/Pool/types.ts
@@ -0,0 +1,47 @@
+import BigNumber from "bignumber.js";
+import type {
+  PoolCategory,
+  PoolConfigBaseProps,
+  SerializedPoolConfig,
+  DeserializedPoolConfig,
+  DeserializedPool,
+  DeserializedPoolVault,
+  DeserializedPoolLockedVault,
+  DeserializedLockedVaultUser,
+  DeserializedLockedCakeVault,
+  SerializedVaultFees,
+  DeserializedVaultFees,
+  DeserializedVaultUser,
+  DeserializedCakeVault,
+} from "@pancakeswap/pools";
+import { VaultKey } from "@pancakeswap/pools";
+
+export interface Address {
+  [chainId: number]: string;
+}
+
+export {
+  PoolCategory,
+  PoolConfigBaseProps,
+  SerializedPoolConfig,
+  DeserializedPoolConfig,
+  DeserializedPoolVault,
+  DeserializedPoolLockedVault,
+  DeserializedPool,
+  DeserializedLockedVaultUser,
+  DeserializedLockedCakeVault,
+  SerializedVaultFees,
+  DeserializedVaultFees,
+  DeserializedVaultUser,
+  DeserializedCakeVault,
+  VaultKey,
+};
+
+export interface HarvestActionsProps {
+  earnings: BigNumber;
+  isLoading?: boolean;
+  onPresentCollect: any;
+  earningTokenPrice: number;
+  earningTokenBalance: number;
+  earningTokenDollarBalance: number;
+}
